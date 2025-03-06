8e04529536a65e9777f5ae909caad6dcf8734f74
Mikkel Gamboa <105696813+Milkz03@users.noreply.github.com>
2025-03-06 13:19:24 +0800

    Feat: add weight consideration to edges

--- STDISCM - Problem 1/GraphConfig.h
+++ STDISCM - Problem 1/GraphConfig.h
@@ -11,13 +11,13 @@
 class GraphConfig {
 public:
 	std::string trim(std::string& str);
-	std::map<std::string, std::vector<std::string>> adjList;
+	std::map<std::string, std::vector<std::pair<std::string, int>>> adjList;
 	std::string fileToExtractConfig;
 	GraphConfig(std::string fileToExtractConfig);
 	void graphContentExtractor(const std::string& graphContent);
 
 	void graphFileReader();
-	void addEdge(const std::string& src, const std::string& dest);
+	void addEdge(const std::string& src, const std::string& dest, int weight);
 };
 
 #endif // GRAPHCONFIG_H
